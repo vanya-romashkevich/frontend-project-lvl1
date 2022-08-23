7927d92c08aff926c66e8df0ff5bd28cb02ade84
Ivan Romashkevich <vanya.romashkevich@gmail.com>
2022-08-23 23:28:53 +0300

    new version 4 step 1 project

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,53 @@ console.log('Welcome to the Brain Games!');
 
 
 import readlineSync from 'readline-sync';
-const name = readlineSync.question('May I have your name? ');
 
-const greeting = console.log(`Hello, ${name}!`);
+//запрос имени
+const nameUser = readlineSync.question('May I have your name? ');
+
+//приветствие
+const greeting = console.log(`Hello, ${nameUser}!`);
 
 export default greeting 
+
+// ДАЛЕЕ ИДЕТ НОВОЕ НУЖНО ПЕРЕНЕСТИ ПО ЗАДАНИЮ В ДРУГОЙ ФАЙЛ
+
+
+// условие игры
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+
+
+
+// функция случайного числа 
+const randomNumber = Math.floor(Math.random() * 100);
+
+// вызов рандомного числа
+const userAnswer = readlineSync.question(`Question: ${randomNumber}\n`);
+
+// ответы
+const positiveAnswer = 'yes';
+const negativeAnswer = 'no';
+
+const opposite = (userAnswer === 'yes' ? 'no' : 'yes');
+const errorMessage = (`${userAnswer} is wrong answer ;(. Correct answer was '${opposite}'. \n Let's try again, ${nameUser}!`);
+
+// игра
+const evenGame = () => {
+    for (let i = 0; i < 3; i += 1) {
+        if (randomNumber % 2 === 0 && userAnswer === positiveAnswer) {
+    console.log('Correct!');
+            } else if (randomNumber % 2 !== 0 && userAnswer === negativeAnswer) {
+    console.log('Correct!');
+            } else if (randomNumber % 2 === 0 && userAnswer === negativeAnswer) {
+    return errorMessage;
+            } else if (randomNumber % 2 !== 0 && userAnswer === positiveAnswer) {
+    return errorMessage;
+            } else {
+    return errorMessage;
+        }
+    }
+    console.log(`Congratulations, ${userName}!`);
+}
+
 
