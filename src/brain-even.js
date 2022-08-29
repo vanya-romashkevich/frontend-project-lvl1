757a11c38f75ff43034981c6570595467f0c7d97
Ivan Romashkevich <vanya.romashkevich@gmail.com>
2022-08-29 21:46:31 +0300

    final version 5 step brain-even

--- src/brain-even.js
+++ src/brain-even.js
@@ -21,32 +21,24 @@ const evenGame = () => {
         // задание со случайным числом юзеру
         const userAnswer = readlineSync.question(`Question: ${randomNumber}\n`);
 
-        // ответы 
-        const positiveAnswer = 'yes';
-        const negativeAnswer = 'no';
-
         // ответ с тернарником для замены в ответе
         const opposite = (userAnswer === 'yes' ? 'no' : 'yes' );
         const errorMessage = (`${userAnswer} is wrong answer ;(. Correct answer was '${opposite}'. \nLet's try again, ${username}!`);
-        
-            if (randomNumber % 2 === 0 && userAnswer === positiveAnswer) {
-    console.log('Correct!');
-            } else if (randomNumber % 2 !== 0 && userAnswer === negativeAnswer) {
-    console.log('Correct!');
-            } else if (randomNumber % 2 === 0 && userAnswer === negativeAnswer) {
-    return console.log(errorMessage);
-            } else if (randomNumber % 2 !== 0 && userAnswer === positiveAnswer) {
-    return console.log(errorMessage);
+
+
+        const isEven = (randomNumber) => randomNumber % 2 === 0
+
+        const expectedAnswer = isEven(randomNumber) ? 'yes' : 'no' 
+            if (expectedAnswer === userAnswer) {
+                console.log('Correct');
+            } else if (expectedAnswer !== userAnswer) {
+                return console.log(errorMessage);
             } else {
-    return console.log(errorMessage);;
-        }
+                return console.log(errorMessage);
+            }
     }
     return console.log(`Congratulations, ${username}!`);
 
-}
-
-
-// есть ошибка : если ввести вместо yes или no рандомный набор букв, независимо от числа всегда будет отвечать, что оно четное.
-// решение - opposite сделать зависимость от того, каким число является (четным или нечетным), а не от ответа юзера (yes or no)
+};
 
 export default evenGame
