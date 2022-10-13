97a8977970015457725d2a90402e5d874d04d577
vanya-romashkevich <vanya.romashkevich@gmail.com>
2022-10-13 15:08:48 +0200

    UPDATE

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 import nameUser from '../src/cli.js';
 
+console.log('Welcome to the Brain Games!');
 nameUser();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const nameUser = () => {
-  const name = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
-  return name;
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default nameUser;
